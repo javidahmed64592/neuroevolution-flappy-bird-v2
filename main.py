bfe21bf846f1f7f1764849914353f1784325bd61
Javid Ahmed <javidahmed@icloud.com>
2024-04-17 21:29:38 +0100

    Add main

---
+++ main.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from flappy_bird.flappy_bird_app import FlappyBirdApp
+from flappy_bird.pg.app import App
+
+if __name__ == "__main__":
+    fbga = FlappyBirdApp.create_and_run(
+        population_size=200,
+        mutation_rate=4,
+        x=30,
+        y=500,
+        size=20,
+        nn_layer_sizes=[1, 2, 2],
+        weights_range=[-1, 1],
+        bias_range=[-1, 1],
+        lifetime=200 * 60,
+    )
+    # fba = App.create_app(name="Flappy Bird", width=1000, height=1000, fps=60, font="freesansbold.ttf", font_size=28)
+    # fba.run()
+# TODO: Add Pipes
+# TODO: Add App
