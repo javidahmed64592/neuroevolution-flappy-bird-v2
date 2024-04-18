dd63db43ddbf316dbe2be82240035f11af8f0397
Javid Ahmed <javidahmed@icloud.com>
2024-04-18 23:35:43 +0100

    Update main

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from __future__ import annotations
 from flappy_bird.flappy_bird_app import FlappyBirdApp
 
 if __name__ == "__main__":
-    fbga = FlappyBirdApp.create_game(
-        name="Flappy Bird", width=600, height=600, fps=60, font="freesansbold.ttf", font_size=24, lifetime=10
+    fba = FlappyBirdApp.create_game(
+        name="Flappy Bird", width=500, height=800, fps=60, font="freesansbold.ttf", font_size=24, lifetime=10
     )
-    fbga.add_ga(
-        population_size=100,
+    fba.add_ga(
+        population_size=200,
         mutation_rate=0.02,
-        x=30,
-        y=200,
-        size=20,
-        nn_layer_sizes=[1, 1, 2],
+        x=40,
+        y=250,
+        size=40,
+        hidden_layer_sizes=[1],
         weights_range=[-1, 1],
         bias_range=[-1, 1],
     )
-    fbga.run()
+    fba.run()
